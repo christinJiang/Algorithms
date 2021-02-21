3e67f2fb368a23db5ca96f06ee961c0cc23f7ff8
jiangjinfeng <jiangjinfengsh@outlook.com>
2021-02-21 14:03:32 +0800

    110

--- Tree/110-isBalancedTree.js
+++ Tree/110-isBalancedTree.js
@@ -18,8 +18,31 @@ const Node = val => ({
   left: null,
   right: null,
 });
-const createTree = array => {
 
+const createTree = array => {
+  if (!Array.isArray(array) || array.length === 0 || array[0] === null) {
+    return null;
+  }
+  const list = [];
+  for (let i = 0; i < array.length; i ++) {
+    list.push(Node(array[i]));
+  }
+  for (let i = 0; i < array.length / 2; i++) {
+    if (2 * i + 1 < array.length) {
+      const leftNode = list[2 * i + 1];
+      if (leftNode.val !== null) {
+        list[i].left = leftNode;
+      }
+    }
+
+    if (2 * i + 2 < array.length) {
+      const rightNode = list[2 * i + 2];
+      if (rightNode.val !== null) {
+        list[i].right = rightNode;
+      }
+    }
+  }
+  return list[0];
 }
 
 const array = [3,9,20,null,null,15,7];
