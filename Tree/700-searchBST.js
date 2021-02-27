8a819e82fe3a34f75e7b0d2c0ef3c84e04ac3f68
jiangjinfeng <jiangjinfengsh@outlook.com>
2021-02-27 09:51:00 +0800

    700

---
+++ Tree/700-searchBST.js
@@ -0,0 +1,58 @@
+var searchBST = function(root, val) {
+
+};
+const Node = val => ({
+  val,
+  left: null,
+  right: null,
+});
+const createBST = array => {
+  if (!Array.isArray(array) || !array.length) {
+    return null;
+  }
+  const length = array.length;
+  const middle = parseInt(length / 2);
+  const list = [];
+  list.push(Node(array[middle]));
+  for (let i = middle - 1, j = middle + 1; i >= 0, j < length; i--, j++) {
+    list.push(Node(array[i]));
+    list.push(Node(array[j]));
+  }
+  if (length % 2 === 0) {
+    list.push(Node(array[0]));
+  }
+  for (let i = 0; i < length / 2; i++) {
+    if (2 * i + 1 < length) {
+      list[i].left = list[2 * i + 1];
+    }
+    if (2 * i + 2 < length) {
+      list[i].right = list[2 * i + 2];
+    }
+  }
+  return list[0];
+};
+
+// const array = [4, 2, 7, 1, 3];
+const array = [1,2,3,4,7];
+const tree = createBST(array);
+console.log('tree', JSON.stringify(tree));
+const result = searchBST(array, 2);
+console.log('result', JSON.stringify(result));
+
+// var arr = [1,2,3,4];
+// var change = arr => {
+//   var length = arr.length;
+//   var middle = parseInt(length / 2);
+//   var result = [];
+//   result.push(arr[middle]);
+//   for(var i=middle -1, j=middle+1;i>=0,j<length;i--,j++) {
+//     result.push(arr[i]);
+//     result.push(arr[j]);
+//   }
+//   if (length%2===0) {
+//     result.push(arr[0]);
+//   }
+//   return result;
+// }
+// let changed = change(arr);
+// console.log(changed);
