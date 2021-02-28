691c56392ce72e21e2fa5d0c2e2823dcbb3eee8b
jiangjinfeng <jiangjinfengsh@outlook.com>
2021-02-28 11:48:13 +0800

    590

---
+++ Tree/590-postorder.js
@@ -0,0 +1,39 @@
+
+var postorder = function(root) {
+  let result = [];
+  const children = root && root.children;
+  if (children) {
+    children.forEach(node => {
+      result = result.concat(postorder(node));
+    });
+  }  
+  if (root.val !== null) {
+    result.push(root.val);
+  }
+  return result;
+};
+const tree = {
+  val: 1,
+  children: [
+    {
+      val: 3,
+      children: [
+        {
+          val: 5,
+        },
+        {
+          val: 6,
+        },
+      ],
+    },
+    {
+      val: 2,
+    },
+    {
+      val: 4,
+    },
+  ],
+};
+
+const result = postorder(tree);
+console.log('result', result);
