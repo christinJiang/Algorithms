ea1158381a73ff8c0149df7afb681bf13ae87b9c
jiangjinfeng <jiangjinfengsh@outlook.com>
2021-02-28 11:34:23 +0800

    589

---
+++ Tree/589-preorder.js
@@ -0,0 +1,42 @@
+/* const tree = {
+  val: 1,
+  children: [
+    {
+      val: 3,
+      children: [
+        {
+          val: 5,
+        },
+        {
+          val: 6,
+        }
+      ],
+    },
+    {
+      val: 2,
+    },
+    {
+      val: 4,
+    }
+  ],
+}; */
+
+const tree = {
+  val: 1, 
+  children
+}
+const preOrder = tree => {
+  let result = [];
+  if (tree && tree.val !== null) {
+    result.push(tree.val);
+  }
+  const children = tree && tree.children;
+  if (children && children.length) {
+    children.forEach(node => {
+      result = result.concat(preOrder(node));
+    });
+  } 
+  return result;
+}
+const result = preOrder(tree);
+console.log('result', result);
