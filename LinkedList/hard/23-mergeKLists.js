e137ab3b19f49f5c026ddb2a2c0b9b28acaeaf9c
jiangjinfeng <jiangjinfengsh@outlook.com>
2021-06-06 10:15:29 +0800

    23

---
+++ LinkedList/hard/23-mergeKLists.js
@@ -0,0 +1,62 @@
+/* 合并K个升序链表
+给你一个链表数组，每个链表都已经按升序排列。
+请你将所有链表合并到一个升序链表中，返回合并后的链表。
+
+示例 1：
+输入：lists = [[1,4,5],[1,3,4],[2,6]]
+输出：[1,1,2,3,4,4,5,6]
+解释：链表数组如下：
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+将它们合并到一个有序链表中得到。
+1->1->2->3->4->4->5->6
+
+示例 2：
+输入：lists = []
+输出：[]
+
+示例 3：
+输入：lists = [[]]
+输出：[]
+ 
+提示：
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] 按 升序 排列
+lists[i].length 的总和不超过 10^4
+*/
+var mergeKLists = function(lists) {
+  const mergeTwoLists = (l1, l2) => {
+    if (!l1) return l2;
+    if (!l2) return l1;
+    const preHeader = new ListNode(-1);
+    let preNode = preHeader;
+    while (l1 && l2) {
+      if (l1.val <= l2.val) {
+        preNode.next = l1;
+        l1 = l1.next;
+      } else {
+        preNode.next = l2;
+        l2 = l2.next;
+      }
+      preNode = preNode.next;
+    }
+    preNode.next = l1 ? l1 : l2;
+    return preHeader.next; 
+  }
+
+  const n = lists.length;
+  if (n === 0) return null;
+  let res = lists[0];
+  for (let i = 1; i < n; i++) {
+    if (lists[i]) {
+      res = mergeTwoLists(res, lists[i]);
+    }
+  } 
+  return res;
+};
